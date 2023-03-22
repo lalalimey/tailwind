facd4d268cebf30b702bbcc9a7b007ff985706b8
lalalimey <verabhat@docchula.com>
2023-03-22 23:10:20 +0700

    first day

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>basic tailwind</title>
+</head>
+<body>
+<div class="bg-red-500">
+    <h1 class="text-emerald-200 text-3xl font-bold underline">
+        Hello world!
+    </h1>
+</div>
+
+</body>
+</html>
